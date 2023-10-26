ef2b56e464897a147378edf53431aa8903bdcd3a
emmydoore <emmaleidoore@gmail.com>
2023-10-26 18:59:59 -0500

    on q7

--- scripts/lahmans_baseball.sql
+++ scripts/lahmans_baseball.sql
@@ -25,9 +25,7 @@ limit 1;
 
 -- 3. Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
 
-select  namefirst as first_name, 
-		namelast as last_name, 
-		sum(salary) as total_salary
+select  namefirst as first_name, namelast as last_name, sum(salary) as total_salary
 from people
 inner join collegeplaying
 using (playerid)
@@ -74,16 +72,34 @@ select Round(sum(so)/sum(g),2) as avg_strikeouts,
 from teams
 where yearid>=1920
 group by decade
-order by decade desc
---Answer:
+order by decade desc;
+--Answer: Average amount of strikeouts increases as time goes on, also it seems like home runs are very rare since only one decade (2000s) had a home run average over 0.00
 
 -- 6. Find the player who had the most success stealing bases in 2016, where __success__ is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted _at least_ 20 stolen bases.
-	
---Answer:
+
+select namefirst as first_name, namelast as last_name, sb as stolen_bases, sum(sb+cs) as total_attempts, (sb*100)/sum(sb+cs)  as successful_attempts_percentage,yearid as year
+from batting
+inner join people
+using (playerid)
+where yearid=2016
+group by first_name, last_name, sb,yearid
+having sum(sb+cs)>=20
+order by successful_attempts_percentage desc;
+--Answer: Chris Owings successfully stole 21 bases or 91% of his attempts
 
 -- 7.  From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
 
+select name as team_name,sum(w) as total_wins, yearid as year,wswin as world_series_win
+from teams
+where yearid between 1970 and 2016
+group by name, yearid,wswin
+order by total_wins desc
 --Answer:
+
+select wswin, t.name,yearid,sum(w)
+from teams as t
+group by t.name, yearid, wswin
+order by name 
 
 -- 8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
 
