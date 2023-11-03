10b9a8ca3c458602ba1c74ff94ee608e70cbe3ce
emmydoore <emmaleidoore@gmail.com>
2023-11-02 21:02:18 -0500

    walkthrough, on 6

--- scripts/lahmans_baseball.sql
+++ scripts/lahmans_baseball.sql
@@ -25,18 +25,19 @@ limit 1;
 
 -- 3. Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
 
-select  namefirst as first_name, namelast as last_name, sum(salary) as total_salary
+select  namefirst as first_name, namelast as last_name, sum(salary)::numeric::money  as total_salary
 from people
-inner join collegeplaying
-using (playerid)
-inner join schools
-using (schoolid)
 inner join salaries
 using (playerid)
-where schoolname='Vanderbilt University'
-group by first_name, last_name
+where playerid in 
+		(select playerid
+		from collegeplaying
+		where schoolid ilike 'vandy')
+		--using a subq here prevents us from joining which is causing the extra years
+group by namefirst, namelast
 order by total_salary desc;
---Answer: David Price, $245,553,888
+--Answer: David Price, $81,851,296.00
+--fixed multiple year issue causeing salary to be tripled
 
 
 -- 4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
@@ -75,9 +76,17 @@ group by decade
 order by decade desc;
 --Answer: Average amount of strikeouts increases as time goes on, also it seems like home runs are very rare since only one decade (2000s) had a home run average over 0.00
 
+--can also do yearid/10*10 for decade
+select Round(sum(so)/sum(g),2) as avg_strikeouts,
+		round(sum(hr)/sum(g),2) as avg_home_runs,
+		yearid/10*10 as decade
+from teams
+where yearid>=1920
+group by decade
+order by decade desc;
 -- 6. Find the player who had the most success stealing bases in 2016, where __success__ is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted _at least_ 20 stolen bases.
 
-select namefirst as first_name, namelast as last_name, sb as stolen_bases, sum(sb+cs) as total_attempts, (sb*100)/sum(sb+cs)  as successful_attempts_percentage,yearid as year
+select namefirst as first_name, namelast as last_name, sb::numeric as stolen_bases, sum(sb::numeric+cs::numeric) as total_attempts, (sb::numeric*100)/sum(sb::numeric+cs::numeric)  as successful_attempts_percentage,yearid as year
 from batting
 inner join people
 using (playerid)
@@ -86,7 +95,7 @@ group by first_name, last_name, sb,yearid
 having sum(sb+cs)>=20
 order by successful_attempts_percentage desc;
 --Answer: Chris Owings successfully stole 21 bases or 91% of his attempts
-
+(sb*100)/sum(sb+cs)
 -- 7.  From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
 
 select name as team_name,sum(w) as total_wins, yearid as year,wswin as world_series_win
